03a8c3d9cb1ee082cdef92e81f23610666c910bf
Road2BigMoney <usaakk99@gmail.com>
2022-09-28 03:04:02 +0300

    copied task and create connection to DB

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import com.mysql.cj.MysqlConnection;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Katata2022ta";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/katascheme";
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        Connection connection;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("СОЕДИНЕНО");
+            }
+            connection.close();
+            if (connection.isClosed()) {
+                System.out.println("ОТСОЕДИНЕНО");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
